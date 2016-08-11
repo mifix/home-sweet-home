a8e1152c064ec2501a61af66f7a2b7054bf0bad5
Christian Mayer <gitlab@mifix.com>
2016-08-11 17:48:17 +0200

    use bootstrap file for all systems

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
 
-if [ ! -f /usr/bin/git ]; then
-	echo " * Getting Xcode command line tools"
-	xcode-select --install
+
+if [ "$(uname)" == "Darwin" ]; then
+
+	if [ ! -f /usr/bin/git ]; then
+		echo " * Getting Xcode command line tools"
+		xcode-select --install
+	fi
+
+
+	if [ ! -f /usr/local/bin/brew ]; then
+		echo " * Installing Homebrew"
+		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	fi
+
+
+	echo " * Installing Ansible"
+	brew install ansible
+
+elif [ "$(uname)" == "Linux" ]; then
+
+	if [ "$(lsb_release -cs)" == "trusty" ]; then
+		echo " * Installing Ansible"
+		sudo add-apt-repository ppa:ansible/ansible
+		sudo apt-get update
+		sudo apt-get -y install ansible
+	fi
+
+
 fi
 
 
-if [ ! -f /usr/local/bin/brew ]; then
-	echo " * Installing Homebrew"
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-
-brew install ansible
